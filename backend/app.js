9e4d7c50505db47cc16196869b6fb1ae2627a7f1
Bamacharan Kundu <bamachrn@gmail.com>
2015-07-14 15:55:49 +0530

    new get method added

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,9 @@ app.use(bodyParser.json());
 
  //all requests for the Books are rediredtes to the bookRouter
  app.use('/api/Books',bookRouter);
-
+ app.get('/',function(req,res){
+         res.send("SomeOne is trying to access the application which is running inside the container");
+ });
 
  app.listen(port,function(){
         console.log('Grunt is running on port: ' + port);
